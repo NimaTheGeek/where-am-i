f4dd31f7fcc7c00a1783d3474b14c889e538668a
Nima M.Farahani <nima_f101@yahoo.com>
2016-08-08 15:42:46 -0400

    setting up first necessities
    location authorization from user

---
+++ where am i/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  where am i
+//
+//  Created by Nima Farahani on 8/8/16.
+//  Copyright © 2016 Nima Farahani. All rights reserved.
+//
+import CoreLocation
+import UIKit
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var manager: CLLocationManager!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //basic setup for any app using map and gps
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy - kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
